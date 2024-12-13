2a1e90251608cba0a42d1de349b36166d2d0b07c
Daniel Silva <diielsilvasousa@gmail.com>
2024-12-13 19:31:01 -0300

    chore: adding styles to draw the card board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, Signal} from '@angular/core';
+import {MatchService} from './services/match.service';
+import {Match} from './models/match';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  private matchService: MatchService = inject(MatchService);
+  protected matchState: Signal<Match> = this.matchService.matchState;
+
+  protected play(position: number): void {
+    this.matchService.play(position);
+  }
+
 }
